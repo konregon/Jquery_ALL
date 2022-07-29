0deee641316be3d331b606f3ff38421912a5aa17
MAULIK RAJENDRA PATEL <maulikpatelmow@gmail.com>
2022-07-29 20:51:49 +0530

    :first

--- selector.js
+++ selector.js
@@ -4,13 +4,21 @@
 //     $("*").css("background", "yellow");
 // });
 
-// 2. #id selector
+// // 2. #id selector
 $(document).ready(function(){
     $("#intro").css("background", "yellow");
 });
 
 
-// 3. .class
+// // 3. .class
 $(document).ready(function(){
     $(".class1").css("background", "blue");
 });
+
+
+// 4. :first
+$(document).ready(function(){
+    $(".first_selector1 > p:first").css("background", "pink");
+});
+
+
